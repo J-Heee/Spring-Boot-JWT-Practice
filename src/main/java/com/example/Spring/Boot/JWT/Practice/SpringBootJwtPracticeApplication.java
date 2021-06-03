9513e8917a16b5858838411290b54b84a6121e1e
devheee <heee7897@gmail.com>
2021-06-03 15:33:19 +0900

    Initial commit

---
+++ src/main/java/com/example/Spring/Boot/JWT/Practice/SpringBootJwtPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.Spring.Boot.JWT.Practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJwtPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJwtPracticeApplication.class, args);
+	}
+
+}
