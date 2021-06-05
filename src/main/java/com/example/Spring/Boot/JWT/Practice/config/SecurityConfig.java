2e22f8d4cba649ebf181bd0a1965af092c00a35a
devheee <heee7897@gmail.com>
2021-06-05 15:34:37 +0900

    ADD: H2 Console 접근

--- src/main/java/com/example/Spring/Boot/JWT/Practice/config/SecurityConfig.java
+++ src/main/java/com/example/Spring/Boot/JWT/Practice/config/SecurityConfig.java
@@ -9,11 +9,21 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
+    public void configure(WebSecurity web) {
+        web
+                .ignoring()
+                .antMatchers(
+                        "/h2-console/**"
+                        ,"/favicon.ico"
+                );
+    }
+
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()    //HttpServletRequest를 사용하는 요청들에 대한 접근제한을 설정
                 .antMatchers("/api/hello").permitAll()    // /api/hello에 대한 요청은 인증없이 접근을 허용
                 .anyRequest().authenticated();    //나머지 요청들은 모두 인증되어야 함
     }
-    
+
 }
