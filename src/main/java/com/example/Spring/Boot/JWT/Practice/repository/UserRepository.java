639c134e072b411f2d27a70be89fdff0cca8c973
devheee <heee7897@gmail.com>
2021-06-07 12:18:38 +0900

    ADD: Repository 관련 코드

---
+++ src/main/java/com/example/Spring/Boot/JWT/Practice/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.example.Spring.Boot.JWT.Practice.repository;
+
+import com.example.Spring.Boot.JWT.Practice.entity.User;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    @EntityGraph(attributePaths = "authorities")
+    Optional<User> findOneWithAuthoritiesByUsername(String username);
+}
